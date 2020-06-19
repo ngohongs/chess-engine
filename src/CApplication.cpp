11681bd419f1a8ebc85c1cf3af757525d9949480
ngohongs <ngohongs@fit.cvut.cz>
2020-06-19 21:13:35 +0200

    help edit

--- src/CApplication.cpp
+++ src/CApplication.cpp
@@ -12,8 +12,8 @@ CApplication::CApplication(const CInterface &interface)
     m_Commands.emplace("play", std::make_unique<CCommandPlay>(m_Interface,
             " [p|c][p|c][1|2|3|4|5] : initialize the game - first letter is the white player, second black player (all must be lowercase), if a game against computer is initialized a number between 1 - 5 is needed for the computer difficulty (example: 'play pc4' for game as white against black computer with difficulty 4)", m_Game));
     m_Commands.emplace("restart", std::make_unique<CCommandRestart>(m_Interface, " : restarts the game", m_Game));
-    m_Commands.emplace("save", std::make_unique<CCommandSave>(m_Interface, " [filename] : saves game to 'filename'", m_Game));
-    m_Commands.emplace("load", std::make_unique<CCommandLoad>(m_Interface, " [filename] : loads 'filename'", m_Game));
+    m_Commands.emplace("save", std::make_unique<CCommandSave>(m_Interface, " [filename] : saves game to 'filename', filenames cannot contain white space", m_Game));
+    m_Commands.emplace("load", std::make_unique<CCommandLoad>(m_Interface, " [filename] : loads 'filename', filenames cannot contain white space", m_Game));
     m_Commands.emplace("move", std::make_unique<CCommandMove>(CCommandMove(m_Interface,
             " [a|b|c|d|e|f|g|h][1|2|3|4|5|6|7|8][a|b|c|d|e|f|g|h]][1|2|3|4|5|6|7|8]][q|r|b|n] : make a move - first two letters is the position of a piece to be move, following two letters is the position of the destination. If a pawn is going to promote, a piece is needed for a pawn to promote to (q, r, b, n). (all must be lowercase) (example: move a1b2)", m_Game)));
     m_Commands.emplace("board", std::make_unique<CCommandBoard>(CCommandBoard(m_Interface, " : shows the state of the chess board", m_Game)));
